072fece41d10ceba45f390dcdafbf967874dd497
Pond Premtoon <varot@mit.edu>
2019-03-01 03:02:24 +0000

    Forgot to add outputting functions

---
+++ yogo/src/DSL.hs
@@ -0,0 +1,50 @@
+module DSL (
+  generateLangFiles
+  , generateGraphFiles
+  ) where
+
+import Data.Proxy ( Proxy(..) )
+import Debug.Trace
+import Data.Typeable
+
+import Data.Comp.Multi
+import Data.List
+import Data.Map ( Map )
+import qualified Data.Map as Map
+
+import Common.Trans
+import Common.DSL
+import Python.DSL
+
+generateNodeLine :: NodeDef -> String
+generateNodeLine (node, args, derives) = "(defnode " ++ node ++ " [" ++ (intercalate " " args) ++ "] " ++ (intercalate " " derives) ++ ")"
+
+generateLangFile :: Namespace -> [NodeDef] -> String
+generateLangFile ns nodes = "(ns " ++ ns ++ ")\n\n" ++ (intercalate "\n" (map generateNodeLine nodes))
+
+generateLangFiles :: (SigToLangDSL sig) => Proxy sig -> Map Namespace String
+generateLangFiles proxy = Map.mapWithKey generateLangFile (sigToDSL proxy)
+
+-- -----------
+-- Graph files
+-- -----------
+
+generateGraphLine :: (NodeToGraphDSL f f) => YGraphEntry f -> String
+generateGraphLine (YGraphNode (E id) (E (Node node)) occurrence) =
+  case nodeForm node of
+    (fn, args) -> idToDSL id ++ " (" ++ fn ++ " " ++ (intercalate " " args) ++ " " ++ occurrenceToDSL occurrence ++ ")"
+generateGraphLine (YGraphEq (E id1) (E id2)) = undefined
+generateGraphLine (YGraphMemSuccessor id1 id2) = undefined
+
+generateGraph :: (NodeToGraphDSL f f) => Name -> YGraph f -> String
+generateGraph (Name name) graph =
+  "(defgraph g-" ++ name ++ "\n  " ++ (intercalate "\n  " $ reverse $ map generateGraphLine graph) ++ ")\n"
+
+generateGraphFile :: (NodeToGraphDSL f f) => Namespace -> YFile f -> String
+generateGraphFile ns graphs =
+  "(ns " ++ ns ++ ")\n\n" ++ (Map.foldlWithKey (\s k g -> s ++ "\n" ++ (generateGraph k g)) "" graphs)
+
+generateGraphFiles :: (NodeToGraphDSL f f) => YProject f -> Map FilePath (Namespace, String)
+generateGraphFiles = (foldl (\m (i, (p, f)) ->
+                               let ns = "graph" ++ show i in
+                                 Map.insert p (ns, generateGraphFile ns f) m) Map.empty) . (zip [0..]) . Map.toList
