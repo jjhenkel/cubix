9b377cec82da365a84bcd2b92e3e0fdf0fa7ce6b
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2019-12-23 12:37:11 -0600

    Updates to work Java examples into our pipeline.

--- examples/java/Main.hs
+++ examples/java/Main.hs
@@ -3,8 +3,15 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
+
+import System.Environment ( getArgs )
+
+import Data.Aeson
+import Data.ByteString.Lazy.UTF8 ( toString )
 
 import Control.Monad ( MonadPlus(..), liftM, (<=<), (>=>) )
 import Control.Monad.Identity ( Identity(..) )
@@ -22,7 +29,6 @@ import           Cubix.Language.Info
 import           Cubix.Language.Java.Parametric.Full
 import qualified Cubix.Language.Java.Parametric.Common as Common
 import qualified Cubix.Language.Java.Parse as Parse
-import Cubix.Language.Parametric.Syntax.Functor ( InsertF(..), ExtractF(..) )
 
 parseFile :: FilePath -> IO (Maybe (JavaTerm CompilationUnitL))
 parseFile path = do
@@ -33,7 +39,7 @@ parseFile path = do
 
 
 vandalize' :: Rewrite JavaTerm IdentL
-vandalize' (project -> (Just (Ident s))) = return $ iIdent (s ++ "_foo")
+vandalize' (project -> (Just (Ident s))) = return $ iIdent (s ++ "Counter")
 
 vandalize :: GRewrite JavaTerm
 vandalize = allbuR $ promoteR $ addFail vandalize'
@@ -52,16 +58,16 @@ liftBlockStmt :: (MonadPlus m) => RewriteM m JavaTerm StmtL -> RewriteM m JavaTe
 liftBlockStmt r (project -> Just (BlockStmt s)) = liftM iBlockStmt (r s)
 liftBlockStmt r _                               = mzero
 
-emptyIsDelete :: (f ~ JavaTerm, ExtractF [] f, InsertF [] f, Monad m, Typeable i) => (f i -> Bool) -> RewriteM m f i -> RewriteM m f [i]
-emptyIsDelete isEmpty r term@(extractF -> (t : ts)) = do
+emptyIsDelete :: (f ~ JavaTerm, Common.ExtractF [] f, Common.InsertF [] f, Monad m, Typeable i) => (f i -> Bool) -> RewriteM m f i -> RewriteM m f [i]
+emptyIsDelete isEmpty r term@(Common.extractF -> (t : ts)) = do
   rewritten <- r t
   return $ if isEmpty t then
-             insertF (rewritten : ts)
+             Common.insertF (rewritten : ts)
            else if isEmpty rewritten then
-             insertF ts
+             Common.insertF ts
            else
-             insertF (rewritten : ts)
-emptyIsDelete isEmpty r (extractF -> []) = return $ insertF []
+             Common.insertF (rewritten : ts)
+emptyIsDelete isEmpty r (Common.extractF -> []) = return $ Common.insertF []
 
 removeWhiles :: GRewrite JavaTerm
 removeWhiles = tryR $ anytdR $ promoteRF $ emptyIsDelete isEmptyBlockStmt $ liftBlockStmt removeWhiles'
@@ -73,14 +79,14 @@ getDeclId :: JavaTerm VarDeclL -> JavaTerm VarDeclIdL
 getDeclId (project -> Just (VarDecl id _)) = id
 
 collectLocalVars :: (MonadWriter [DeclSpec] m) => TranslateM m JavaTerm BlockStmtL ()
-collectLocalVars (project -> (Just (LocalVars mods typ decls))) = tell (map (DeclSpec mods typ) $ map getDeclId $ extractF decls)
+collectLocalVars (project -> (Just (LocalVars mods typ decls))) = tell (map (DeclSpec mods typ) $ map getDeclId $ Common.extractF decls)
 collectLocalVars _                                              = return ()
 
 collectDecls :: (MonadWriter [DeclSpec] m) => GTranslateM m JavaTerm ()
 collectDecls = mtryM . (promoteTF $ addFail collectLocalVars)
 
 varDeclIdToLhs :: JavaTerm VarDeclIdL -> JavaTerm LhsL
-varDeclIdToLhs (project -> Just (VarId id)) = iNameLhs $ iName $ insertF [id]
+varDeclIdToLhs (project -> Just (VarId id)) = iNameLhs $ iName $ Common.insertF [id]
 varDeclIdToLhs (project -> Just (VarDeclArray inner)) = varDeclIdToLhs inner
 
 varDeclArrayDim :: JavaTerm VarDeclIdL -> Int
@@ -88,16 +94,16 @@ varDeclArrayDim (project -> Just (VarId id))          = 0
 varDeclArrayDim (project -> Just (VarDeclArray inner)) = 1 + varDeclArrayDim inner
 
 declToAssn :: JavaTerm TypeL -> JavaTerm VarDeclL -> [JavaTerm ExpL]
-declToAssn _ (project -> Just (VarDecl _ (extractF -> Nothing)))    = []
-declToAssn t (project -> Just (VarDecl id (extractF -> Just init))) =
+declToAssn _ (project -> Just (VarDecl _ (Common.extractF -> Nothing)))    = []
+declToAssn t (project -> Just (VarDecl id (Common.extractF -> Just init))) =
   case project init of
     Just (InitExp e)     -> [iAssign (varDeclIdToLhs id) iEqualA e]
     Just (InitArray arr) -> [iAssign (varDeclIdToLhs id) iEqualA (iArrayCreateInit t (varDeclArrayDim id) arr)]
 
 declToInit :: (Monad m) => RewriteM m JavaTerm [BlockStmtL]
-declToInit (extractF -> (project -> Just (LocalVars _ t decls)) : stmts) = return $ insertF (assnStmts ++ stmts)
+declToInit (Common.extractF -> (project -> Just (LocalVars _ t decls)) : stmts) = return $ Common.insertF (assnStmts ++ stmts)
   where
-    assns = concatMap (declToAssn t) (extractF decls)
+    assns = concatMap (declToAssn t) (Common.extractF decls)
 
     assnStmts :: [JavaTerm BlockStmtL]
     assnStmts = map (iBlockStmt . iExpStmt) assns
@@ -108,11 +114,11 @@ forDeclToInit = undefined
 
 
 reifyDeclSpec :: DeclSpec -> JavaTerm BlockStmtL
-reifyDeclSpec (DeclSpec mods typ id) = iLocalVars mods typ (insertF [iVarDecl id (insertF Nothing)])
+reifyDeclSpec (DeclSpec mods typ id) = iLocalVars mods typ (Common.insertF [iVarDecl id (Common.insertF Nothing)])
 
 -- Note: This misses constructor bodies
 prependDecls :: (Monad m) => [DeclSpec] -> RewriteM m JavaTerm BlockL
-prependDecls decls (project -> (Just (Block stmts))) = return $ iBlock $ insertF $ (map reifyDeclSpec decls) ++ (extractF stmts)
+prependDecls decls (project -> (Just (Block stmts))) = return $ iBlock $ Common.insertF $ (map reifyDeclSpec decls) ++ (Common.extractF stmts)
 
 isBlock :: (Monad m) => GRewriteM (MaybeT m) JavaTerm
 isBlock = promoteRF (idR :: RewriteM _ JavaTerm BlockL)
@@ -134,10 +140,23 @@ declsToFront = alltdR $ promoteR $ addFail declsToFront'
 printJava :: JavaTerm CompilationUnitL -> IO ()
 printJava = putStrLn . prettyPrint . untranslate
 
+data TransformResult = TransformResult {transform :: String, result :: String}
+
+instance ToJSON TransformResult where
+  toJSON TransformResult{..} = object [
+    "transform" .= transform,
+    "result"    .= result  ]
+
+runT = prettyPrint . untranslate . Common.untranslate . Common.translate . runIdentity
+
 main = do
-  x <- parseFile "foo.java"
+  [name] <- getArgs
+  x <- parseFile name
   case x of
     Nothing -> putStrLn "Parse failed"
-    Just t  -> do let t' = Common.translate t
-                  print t'
-                  printJava $ Common.untranslate t'
+    Just t  -> do 
+      putStrLn $ toString $ encode $
+        [ TransformResult "removeWhiles" (runT $ removeWhiles t)
+        , TransformResult "declsToFront" (runT $ declsToFront t)
+        , TransformResult "vandilize" (runT $ vandalize t) ]
+    
